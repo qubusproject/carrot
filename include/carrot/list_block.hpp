82f5ab742862d30f6323cdf8caa1265d2d010b1d
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2016-02-29 14:41:14 +0100

    Add a copyright notice to every file

--- include/carrot/list_block.hpp
+++ include/carrot/list_block.hpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2015-2016 Christopher Hinz
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef CARROT_LIST_BLOCK_HPP
 #define CARROT_LIST_BLOCK_HPP
 
--- include/carrot/text_block.hpp
+++ include/carrot/text_block.hpp
@@ -1,3 +1,8 @@
+//  Copyright (c) 2015-2016 Christopher Hinz
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef CARROT_TEXT_BLOCK_HPP
 #define CARROT_TEXT_BLOCK_HPP
 
