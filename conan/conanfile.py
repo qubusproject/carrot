fcad61744d403bbd9429a2921821b56592b95dce
Christopher Hinz <hinz@theo-physik.uni-kiel.de>
2018-10-24 12:06:28 +0200

    Add ICU as a dependency

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -9,12 +9,14 @@ class CarrotConan(ConanFile):
     description = "A C++ library for rendering expressive diagnostic messages"
     settings = "os", "compiler", "build_type", "arch", "cppstd"
     generators = "cmake_paths"
-    requires = "boost/1.67.0@conan/stable"
+    requires = "boost/1.67.0@conan/stable", "icu/62.1@bincrafters/stable"
     exports_sources = "../*"
 
     def configure(self):
         if self.settings.compiler != "Visual Studio" and not self.options["boost"].shared:
             self.options["boost"].fPIC = True
+
+        self.options["icu"].data_packaging = "library"
 
     def build(self):
         cmake = CMake(self)
