2445fbfaefa5372007bb1972ef10edbc6308ea44
Christopher Hinz <chinz07@qubusproject.org>
2018-11-06 23:07:38 +0100

    Explicitly cast the result of distance to silence a conversion warning

---
+++ include/carrot/integers.hpp
@@ -0,0 +1,15 @@
+#ifndef CARROT_INTEGERS_HPP
+#define CARROT_INTEGERS_HPP
+
+namespace carrot
+{
+
+template <typename Destination, typename Source>
+constexpr Destination integer_cast(const Source& value)
+{
+    return static_cast<Destination>(value);
+}
+
+} // namespace carrot
+
+#endif
