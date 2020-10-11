b0b61509dd31565d902bec7e4e6065c35446789e
Christopher Hinz <chinz07@qubusproject.org>
2020-10-11 15:06:02 +0200

    Fix the build pipeline
    As part of the fix, the test image has been updated to
    Ubuntu 20.04 since 19.04 is EOL.
    
    This commit also disables Unicode support on
    Windows since it is currently broken.

--- conanfile.py
+++ conanfile.py
@@ -11,6 +11,8 @@ class CarrotConan(ConanFile):
     generators = "cmake_find_package_multi"
     requires = "boost/1.74.0", "icu/67.1", "fmt/7.0.3"
     exports_sources = "../*"
+    options = {"with_unicode": [True, False]}
+    default_options = {"with_unicode": False}
 
     def configure(self):
         self.options["icu"].data_packaging = "library"
@@ -18,8 +20,9 @@ class CarrotConan(ConanFile):
     def build(self):
         cmake = CMake(self)
 
-        cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = "conan_paths.cmake"
         cmake.definitions["CARROT_BUILD_TESTS"] = "True"
+
+        cmake.definitions["CARROT_WITH_UNICODE_SUPPORT"] = self.options["with_unicode"]
 
         cmake.configure(build_folder="./build")
         cmake.build()
