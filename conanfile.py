a593f7e4cb4808105d60680f3da76772c0e99dc9
Christopher Hinz <chinz07@qubusproject.org>
2024-10-12 00:35:59 +0200

    Fix tests

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,5 @@
 from conan import ConanFile
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
 
 class CarrotConan(ConanFile):
     name = "carrot"
@@ -8,23 +8,29 @@ class CarrotConan(ConanFile):
     url = "https://github.com/qubusproject/carrot"
     description = "A C++ library for rendering expressive diagnostic messages"
     settings = "os", "compiler", "build_type", "arch"
-    requires = "boost/1.86.0", "icu/[~75]", "fmt/[~11]"
-    exports_sources = "../*"
-    generators = "CMakeDeps", "CMakeToolchain"
+    requires = "boost/[>=1.86]", "icu/[~75]", "fmt/[~11]"
+    test_requires = "catch2/3.7.1"
+    exports_sources = "src/*", "tests/*", "examples/*", "doc/*", "cmake/*", "CMakeLists.txt", "README.md", "LICENSE"
     options = {"with_unicode": [True, False]}
     default_options = {"with_unicode": False}
 
     def configure(self):
         self.options["icu"].data_packaging = "library"
 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["CARROT_BUILD_TESTS"] = "True"
+        tc.variables["CARROT_WITH_UNICODE_SUPPORT"] = self.options["with_unicode"]
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.build_context_activated = ["catch2"]
+        deps.generate()
+
     def build(self):
         cmake = CMake(self)
 
-        cmake.definitions["CARROT_BUILD_TESTS"] = "True"
-
-        cmake.definitions["CARROT_WITH_UNICODE_SUPPORT"] = self.options["with_unicode"]
-
-        cmake.configure(build_folder="./build")
+        cmake.configure()
         cmake.build()
 
         cmake.install()
